b76f3213d351839c28875bd39ee628a193ecfb31
Jeropis <jerocaru@gmail.com>
2023-01-25 08:50:06 +0100

    Creado proyecto y repo

---
+++ Academent/app/src/main/java/academent/proyecto2t/jerocr/ActividadPrincipal.kt
@@ -0,0 +1,11 @@
+package academent.proyecto2t.jerocr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActividadPrincipal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_principal)
+    }
+}
